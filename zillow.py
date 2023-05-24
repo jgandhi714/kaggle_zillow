98790069e9c37fca073738c5bb4d3b9440e9c368
jgandhi714 <jgandhi714@gmail.com>
2023-05-24 14:20:35 -0400

    impute decktypeid with 0 rather than drop

--- zillow.py
+++ zillow.py
@@ -18,11 +18,11 @@ PROPERTIES_DF_CATEGORY_COLUMNS = ['airconditioningtypeid', 'buildingqualitytypei
 PROPERTIES_DF_REDUNDANT_COLUMNS_TO_DROP = ['calculatedbathnbr', 'poolcnt', 'hashottuborspa', 'taxdelinquencyyear',
                                            'rawcensustractandblock', 'assessmentyear']
 # properties with mostly null values that can't be reliably imputed
-PROPERTIES_DF_NULL_COLUMNS_TO_DROP = ['architecturalstyletypeid', 'basementsqft', 'buildingclasstypeid', 'decktypeid',
+PROPERTIES_DF_NULL_COLUMNS_TO_DROP = ['architecturalstyletypeid', 'basementsqft', 'buildingclasstypeid',
                                       'finishedfloor1squarefeet', 'finishedsquarefeet6', 'finishedsquarefeet13',
                                       'finishedsquarefeet15', 'finishedsquarefeet50', 'poolsizesum', 'storytypeid',
                                       'threequarterbathnbr', 'typeconstructiontypeid', 'yardbuildingsqft26', 'numberofstories']
-ZERO_IMPUTATION_COLUMNS = ['fireplacecnt', 'pooltypeid2', 'pooltypeid7', 'pooltypeid10']
+ZERO_IMPUTATION_COLUMNS = ['fireplacecnt', 'pooltypeid2', 'pooltypeid7', 'pooltypeid10', 'decktypeid']
 ONE_IMPUTATION_COLUMNS = ['fullbathcnt', 'unitcnt', 'bedroomcnt', 'bathroomcnt']
 MODE_IMPUTATION_COLUMNS = PROPERTIES_DF_CATEGORY_COLUMNS
 IMPUTE_X_COLUMNS = ZERO_IMPUTATION_COLUMNS + ONE_IMPUTATION_COLUMNS + MODE_IMPUTATION_COLUMNS
